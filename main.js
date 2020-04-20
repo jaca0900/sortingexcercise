d02a4d4f59241c3b80931cdd915bbb79a80ba7bc
Jacek Bednarczyk <jaca09001@gmail.com>
2020-04-20 16:49:27 +0200

    Added small formating for main program file

--- main.js
+++ main.js
@@ -4,4 +4,4 @@ import ArrayUtils from './utils/arrayUtils.js';
 const sortArray = [1, 9, 5, 13, 3, 11, 7, 15, 2, 10, 6, 14, 4, 12, 8, 16];
 const sorted = ArrayUtils.pullSort(sortArray);
 
-console.log(`Array was sorted with the outcome: ${sorted}`);
+console.log(`Array:\n ${sortArray}\nwas sorted with the outcome:\n ${sorted}`);
